bb13c9baed209a74e9f0ef5f90690aa766ddd87c
André Freitas <dre.freitas23@gmail.com>
2025-08-04 23:10:09 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the main page of our application.</p>
+      <Button>Teste</Button>
     </div>
   );
 };
